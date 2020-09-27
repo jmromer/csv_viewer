d28a34ee9a5f028ecd62ceb1a5351fd05f9c6d3e
Jake Romer <mail@jakeromer.com>
2020-09-27 15:10:46 -0400

    [web] Drop strict mode, inline base css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,26 +1,9 @@
 import App from 'components/App';
-import 'index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from 'serviceWorker';
-import api from 'api'
 
-declare global {
-  interface Window {
-    list : any;
-    csv : any;
-    create : any;
-  }
-}
-
-api.csvList().then(list => window.list = list)
-api.csvFind(1).then(csv => window.csv = csv)
-window.create = api.csvCreate
-
-ReactDOM.render(
-  <React.StrictMode><App /></React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
