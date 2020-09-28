803a2cc5f937711d898aae24ed8794c2c7c47449
Jake Romer <mail@jakeromer.com>
2020-09-28 01:05:43 -0400

    [api] Update pytest config

--- api/tests.py
+++ api/tests.py
@@ -1,8 +1,32 @@
+import pytest
+
+
 def test_root_url(client):
-    resp = client.get("/")
+    resp = client.get('/')
     assert resp.status_code == 200
 
 
+@pytest.mark.skip
 def test_post_csv_success(client):
-    resp = client.get("/csvs")
+    resp = client.post('/api/csv-files')
+    assert resp.status_code == 200
+
+
+@pytest.mark.skip
+def test_delete_csv_success(client):
+    csv_id = 1
+    resp = client.delete(f'/api/csv-files/{csv_id}')
+    assert resp.status_code == 200
+
+
+@pytest.mark.skip
+def test_get_csv_success(client):
+    csv_id = 1
+    resp = client.get(f'/api/csv-files/{csv_id}')
+    assert resp.status_code == 200
+
+
+@pytest.mark.skip
+def test_get_csv_list_success(client):
+    resp = client.get('/api/csv-files/')
     assert resp.status_code == 200
